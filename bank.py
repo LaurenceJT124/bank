9db999fbd6f46f80e70a01a3c66f65e628f057f6
laurence <lthom124@charlotte.edu>
2025-06-26 23:33:05 -0400

    savings done

--- bank.py
+++ bank.py
@@ -1,12 +1,15 @@
 class BankAccount:
 
-    title = "BigBank"
+    title = "big_bank"
 
-    def __init__(self, customer_name, current_balance, minimum_balance):
-        # Instance attributes
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, interest_rate):
+
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.interest_rate = interest_rate
 
     def deposit(self, amount):
         self.current_balance += amount
@@ -22,3 +25,6 @@ class BankAccount:
         print(f"Customer Name: {self.customer_name}")
         print(f"Current Balance: {self.current_balance}")
         print(f"Minimum Balance: {self.minimum_balance}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Routing Number: {self.routing_number}")
+        print(f"Interest Rate: {self.interest_rate}")
